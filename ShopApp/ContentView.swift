b08a7da08b9060aa52ce36e4d7c94d2f54714637
Kim Nari <kimnari@gimnaliui-MacBookPro.local>
2022-09-17 19:04:49 +0900

    first commit

---
+++ ShopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopApp
+//
+//  Created by Kim Nari on 2022/09/17.
+//
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Kim Nari on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
